d5f9efe5213b8818018240463b7fb67b468c0518
Umisyo <s_kusunoki@yumemi.co.jp>
2019-10-04 17:53:10 +0900

    'refactor: update stylelint'

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,5 +1,10 @@
 module.exports = {
-  plugins: ['stylelint-order', 'stylelint-scss'],
+  plugins: [
+    'stylelint-order',
+    'stylelint-scss',
+    'stylelint-no-unsupported-browser-features',
+    'stylelint-declaration-block-no-ignored-properties',
+  ],
   extends: [
     'stylelint-config-standard',
     './node_modules/prettier-stylelint/config.js',
@@ -7,8 +12,30 @@ module.exports = {
   ignoreFiles: ['**/node_modules/**'],
   rules: {
     indentation: 2,
+    'color-no-invalid-hex': true,
+    'font-family-no-duplicate-names': true,
+    'font-family-no-missing-generic-family-keyword': true,
+    'declaration-block-trailing-semicolon': 'always',
     'string-quotes': 'single',
     'at-rule-no-unknown': null,
+    'plugin/declaration-block-no-ignored-properties': true,
+    'plugin/no-unsupported-browser-features': [
+      true,
+      {
+        browsers: [
+          '> 5% in JP',
+          'Last 2 Safari versions',
+          'Last 2 iOS versions',
+          'Last 2 Chrome versions',
+          'Last 2 ChromeAndroid versions',
+          'IE 11',
+          'Last 2 Edge versions',
+          'Last 2 Firefox versions',
+          'Last 2 FirefoxAndroid versions',
+        ],
+        severity: ['warning'],
+      },
+    ],
     'at-rule-no-unknown': [
       true,
       {
@@ -28,4 +55,3 @@ module.exports = {
     'order/properties-alphabetical-order': true,
   },
 }
-
